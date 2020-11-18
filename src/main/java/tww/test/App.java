f5f813b76dc957719d671d7acfc9b3cd0e72128a
tuweiwei <794692685@qq.com>
2020-11-18 14:36:48 +0800

    first commit

---
+++ src/main/java/tww/test/App.java
@@ -0,0 +1,13 @@
+package tww.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
